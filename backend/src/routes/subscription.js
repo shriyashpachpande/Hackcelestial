f0b78e5ea2207dab78ae7bc404a08b8450542c96
shriyashpachpande <shriyashpachpande813@gmail.com>
2025-09-20 15:49:30 +0530

    Update subscription.js

--- backend/src/routes/subscription.js
+++ backend/src/routes/subscription.js
@@ -5,7 +5,7 @@
 // import UserSubscription from "../models/UserSubscription.js";  // MongoDB model
 // import { getCustomerIdForUser } from "../utils/db.js";         // Helper function to get customerId
 // import nodemailer from "nodemailer";
-
+import UserSubscription from "../models/UserSubscription.js";
 // dotenv.config();
 // const router = express.Router();
 // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
